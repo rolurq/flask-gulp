2c9a83b487b0738cc2e6d575a485f018972cb070
Rolando Urquiza <rolurquiza@gmail.com>
2016-07-31 01:44:02 -0400

    Fix watcher loop

--- watcher.py
+++ watcher.py
@@ -1,27 +1,30 @@
+import os
+
 from threading import Thread
 from werkzeug._reloader import ReloaderLoop
 
+
 class Watcher(Thread, ReloaderLoop):
 
-    def __init__(self, dirname, interval=1, static, tasks, *args, **kwargs):
-        self.dirname = dirname
+    def __init__(self, paths, static, tasks, interval=1, *args, **kwargs):
+        self.paths = paths
         self.static = static
         self.tasks = tasks
         super(Watcher, self).__init__(*args, **kwargs)
         ReloaderLoop.__init__(self, interval=interval)
 
     def run(self):
-        time = None
-        while True:
-            try:
-                mtime = os.stat(dirname).st_mtime
-            except OSError:
-                continue
+        times = {}
+        while not self._Thread__stopped:
+            for filename in self.paths:
+                try:
+                    currtime = os.stat(filename).st_mtime
+                except OSError:
+                    continue
 
-            if time is None:
-                time = mtime
-            elif mtime > time:
-                self.static.run(self.tasks)
-                break
+                oldtime = times.get(filename)
+                if oldtime and currtime > oldtime:
+                    self.static.run(*self.tasks)
+                times[filename] = mtime
             self._sleep(self.interval)
 
