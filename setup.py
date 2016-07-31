f1b544f3799a470bf529cf3601a1bf836c8e4645
Rolando Urquiza <rolurquiza@gmail.com>
2016-07-31 12:08:25 -0400

    Add version information file

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
 
+def get_version():
+    with open('version') as f:
+        return f.read()
+
 setup(name='Flask-Static', license='MIT', author='Rolando Urquiza',
-      author_email='rolurquiza@gmail.com',
+      author_email='rolurquiza@gmail.com', version=get_version(),
       description='Task executioner similar to gulp for Python',
       packages=['flask_static'], platforms='any',
       install_requires=['werkzeug'],
