1c0404c93dda555a627c165ef6adb21c000c535f
Rolando Urquiza <rolurquiza@gmail.com>
2016-08-01 01:06:57 -0400

    Set run on refresh false by default

--- flask_static/static.py
+++ flask_static/static.py
@@ -25,7 +25,7 @@ class Static(object):
         app.config.setdefault('STATIC_WATCHER_INTERVAL', 1)
         app.config.setdefault('STATIC_INITIAL_PATH', app.root_path)
         app.config.setdefault('STATIC_GENERATED_LINKS_PATH', app.static_folder)
-        app.config.setdefault('STATIC_RUN_ON_REFRESH', True)
+        app.config.setdefault('STATIC_RUN_ON_REFRESH', False)
         self.app = app
 
         @app.context_processor
