6e351629000a65c7ecc68b14ce68ca5a339bfa93
Rolando Urquiza <rolurquiza@gmail.com>
2016-09-19 22:29:55 -0400

    Add package desciptions

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 import flask_gulp
 
@@ -10,10 +13,13 @@ setup(name='Flask-Gulp', license='MIT License', author='Rolando Urquiza',
       author_email='rolurquiza@gmail.com', version=version,
       keywords='flask gulp task watcher',
       description='Task executioner similar to gulp for Python',
+      long_description=readme,
       packages=['flask_gulp'], platforms='any',
       install_requires=['flask', 'werkzeug'],
       classifiers=['Environment :: Web Environment',
                    'Intended Audience :: Developers',
                    'Operating System :: OS Independent',
-                   'Programming Language :: Python'],
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'],
       url='https://github.com/rolurq/flask-gulp')
