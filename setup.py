14c560d7250a91fcaf7feb98cb4e680d8e16effd
Rolando Urquiza <rolurquiza@gmail.com>
2016-09-19 22:29:26 -0400

    Extract version from module

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+import flask_gulp
 
-setup(name='Flask-Gulp', license='MIT', author='Rolando Urquiza',
-      author_email='rolurquiza@gmail.com', version='0.2.5',
+
+readme = open('README').read()
+version = flask_gulp.__version__
+
+setup(name='Flask-Gulp', license='MIT License', author='Rolando Urquiza',
+      author_email='rolurquiza@gmail.com', version=version,
+      keywords='flask gulp task watcher',
       description='Task executioner similar to gulp for Python',
       packages=['flask_gulp'], platforms='any',
       install_requires=['flask', 'werkzeug'],
